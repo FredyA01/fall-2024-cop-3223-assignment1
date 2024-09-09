a7645f75835e77871b70647bb162929dff6afe6a
Fred01 <fr827712@ucf.edu>
2024-09-09 03:20:40 +0000

    pray to the Lord

--- fracturing.c
+++ fracturing.c
@@ -19,7 +19,13 @@ printf("result: %f\n", distance);
     return 0;
 }
 double calculateDistance() {
-double x1 = 2, y1 = 4, x2 = 5, y2 = 8;
+double x1, x2, y1, y2;
+    // Prompt user for input
+    printf("Enter coordinates of the first point (x1 y1): ");
+    scanf("%lf %lf", &x1, &y1);
+
+    printf("Enter coordinates of the second point (x2 y2): ");
+    scanf("%lf %lf", &x2, &y2);
 double dx = x2 - x1;
 double dy = y2 - y1;
 double px = pow(dx, 2);
