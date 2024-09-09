d5aaa9922e0072b99983e31665f3ff680389fd9b
Fred01 <fr827712@ucf.edu>
2024-09-09 04:35:59 +0000

    good draft

--- fracturing.c
+++ fracturing.c
@@ -3,9 +3,9 @@
 
 double calculateDistance(double x1, double y1, double x2, double y2);
 double calculatePerimeter(double distance);
-double calculateArea();
-double calculateWidth();
-double CalculateHeight();
+double calculateArea(double distance);
+double calculateWidth(double x1, double x2);
+double calculateHeight(double y1, double y2);
 
 //Define the value of Pi
 #define Pi  3.14159
@@ -21,11 +21,17 @@ int main(int argc, char **argv) {
 
     double distance = calculateDistance(x1, y1, x2, y2);
     double perimeter = calculatePerimeter(distance);
-    
+    double area = calculateArea(distance);
+    double width = calculateWidth(x1, x2);
+    double height = calculateHeight(y1, y2);
+
     printf("Point #1 entered: (%f, %f)\n", x1, y1);
     printf("Point #2 entered: (%f, %f)\n", x2, y2);
     printf("distance: %f\n", distance);
     printf("circumference: %f\n", perimeter);
+    printf("Area: %f\n", area);
+    printf("Width: %f\n", width);
+    printf("Height: %f\n", height);
 
     return 0;
 }
@@ -34,9 +40,7 @@ double calculateDistance(double x1, double y1, double x2, double y2) {
     // Calculation using Distance Formula
     double dx = x2 - x1;
     double dy = y2 - y1;
-    double px = pow(dx, 2);
-    double py = pow(dy, 2);
-    double distance = (sqrt(px+py));
+    double distance = (sqrt(dx * dx + dy * dy));
 
     return distance;
 }
@@ -46,5 +50,22 @@ double calculatePerimeter(double distance) {
     double radius = distance / 2;
     double perimeter = 2 * Pi * radius;
 
-    return 1.0;
+    return perimeter;
+}
+
+double calculateArea(double distance){
+    double radius = distance / 2;
+    double area = Pi * pow(radius, 2);
+
+    return area;
+}
+
+double calculateWidth(double x1, double x2){
+
+    return x2 - x1;
+}
+
+double calculateHeight(double y1, double y2){
+ 
+    return y2 - y1;
 }
