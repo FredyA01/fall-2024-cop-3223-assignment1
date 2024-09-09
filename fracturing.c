3f373520c4710fafb51372afb2c8fc83762f3108
Fred01 <fr827712@ucf.edu>
2024-09-09 03:12:12 +0000

    fffffff

--- fracturing.c
+++ fracturing.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<math.h>
 
-double calculatedistance();
+double calculateDistance();
 double calculatePerimeter();
 double calculateArea();
 double calculateWidth();
@@ -10,10 +10,10 @@ double CalculateHeight();
 //Define the value of Pi
 #define Pi  3.14159
 
-int main(int agrc, char **argv) {
+int main(int argc, char **argv) {
 
-double calculateDistance();
-
+double distance = calculateDistance();
+printf("result: %f\n", distance);
 
 
     return 0;
@@ -25,6 +25,5 @@ double dy = y2 - y1;
 double px = pow(dx, 2);
 double py = pow(dy, 2);
 double distance = (sqrt(px+py));
-printf ("result: %f\n", distance);
 return distance;
 }
