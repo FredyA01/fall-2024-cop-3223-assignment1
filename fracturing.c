96c2a4ccfc3927be151deff8a24c88064eca9909
Fred01 <fr827712@ucf.edu>
2024-09-09 03:49:10 +0000

    i did it

--- fracturing.c
+++ fracturing.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #include<math.h>
 
-double calculateDistance();
-double calculatePerimeter();
+double calculateDistance(double x1, double y1, double x2, double y2);
+double calculatePerimeter(double distance);
 double calculateArea();
 double calculateWidth();
 double CalculateHeight();
@@ -11,25 +11,39 @@ double CalculateHeight();
 #define Pi  3.14159
 
 int main(int argc, char **argv) {
-
-double distance = calculateDistance();
-printf("result: %f\n", distance);
-
-
-    return 0;
-}
-double calculateDistance() {
-double x1, x2, y1, y2;
-    // Prompt user for input
+    double x1, y1, x2, y2;
+  // Prompt user for input
     printf("Enter coordinates of the first point (x1 y1): ");
     scanf("%lf %lf", &x1, &y1);
 
     printf("Enter coordinates of the second point (x2 y2): ");
     scanf("%lf %lf", &x2, &y2);
-double dx = x2 - x1;
-double dy = y2 - y1;
-double px = pow(dx, 2);
-double py = pow(dy, 2);
-double distance = (sqrt(px+py));
-return distance;
+
+    double distance = calculateDistance(x1, y1, x2, y2);
+    printf("distance: %f\n", distance);
+    double perimeter = calculatePerimeter(distance);
+    printf("circumference: %f\n", perimeter);
+    return 0;
+}
+double calculateDistance(double x1, double y1, double x2, double y2) {
+    double x1, x2, y1, y2;
+
+  
+
+    // Calculation using Distance Formula
+    double dx = x2 - x1;
+    double dy = y2 - y1;
+    double px = pow(dx, 2);
+    double py = pow(dy, 2);
+    double distance = (sqrt(px+py));
+
+    return distance;
+}
+
+double calculatePerimeter(double distance) {
+
+    double radius = distance / 2;
+    double perimeter = 2 * Pi * radius;
+
+    return perimeter;
 }
